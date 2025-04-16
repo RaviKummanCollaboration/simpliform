ba4750299cad9d3f26fa9ba833f660881390e242
kummancoder <kummandhakad@gmail.com>
2025-04-16 23:18:06 +0530

    HomePage component updated (HeroSection)

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,8 +1,15 @@
+import { Footer } from '@/components/Footer'
+import HeroSection from '@/components/HeroSection'
 import React from 'react'
 
 const HomePage = () => {
   return (
-    <div>Home</div>
+    <div>
+      <div className='grid items-center justify-items-center min-h-screen p-8 gap-16 sm:p-20'>
+        <HeroSection/>
+        <Footer/>
+    </div>
+    </div>
   )
 }
 
